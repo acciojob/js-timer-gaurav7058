47c935af9a776eb80b0b71a9249aec67335684ac
gaurav7058 <146345401+gaurav7058@users.noreply.github.com>
2024-10-14 14:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-// write js code here if required
+const date=new Date();
+const year=date.getFullYear();
+const month=String(date.getMonth()+1).padStart(2,'0');
+const day=String(date.getDate()).padStart(2,'0');
+const hour=date.getHours() 
+const min=date.getMinutes()
+const sec=date.getSeconds()
+
+const timer=document.getElementById("timer");
+
+timer.innerHTML=`${day}/${month}/${year}, ${hour}:${min}:${sec}`
